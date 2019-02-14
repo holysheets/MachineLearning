98323aa7c051e099bdcd5b577e8ba9980fa74156
holysheets <47127171+holysheets@users.noreply.github.com>
2019-02-13 22:18:44 -0700

    Add files via upload

--- DecisionTree/run.sh
+++ DecisionTree/run.sh
@@ -1,1 +1,14 @@
 
+
+python id3.py --train car/train.csv --test car/test.csv --car --maxDepth 3
+python id3.py --train car/train.csv --test car/test.csv --car --maxDepth 6
+python id3.py --train car/train.csv --test car/test.csv --car --gini --maxDepth 3
+python id3.py --train car/train.csv --test car/test.csv --car --gini --maxDepth 6
+python id3.py --train car/train.csv --test car/test.csv --car --me --maxDepth 3
+python id3.py --train car/train.csv --test car/test.csv --car --me --maxDepth 6
+python id3.py --train bank/train.csv --test bank/test.csv --bank --maxDepth 3
+python id3.py --train bank/train.csv --test bank/test.csv --bank --maxDepth 16
+python id3.py --train bank/train.csv --test bank/test.csv --bank --gini --maxDepth 3
+python id3.py --train bank/train.csv --test bank/test.csv --bank --gini --maxDepth 16
+python id3.py --train bank/train.csv --test bank/test.csv --bank --me --maxDepth 3
+python id3.py --train bank/train.csv --test bank/test.csv --bank --me --maxDepth 16
